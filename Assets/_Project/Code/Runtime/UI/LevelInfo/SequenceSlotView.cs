259ddec2664c82bc7aca4184bda6baaeb261062e
Leonid Medvedev <77212731+LeonPucin@users.noreply.github.com>
2025-11-02 01:08:34 +0300

    Add new motion icons and update UI prefabs
    Added several new motion icon textures and their meta files to the project. Updated SequenceSlotView to support an additional icon background for movement slots. Modified Card and CardSetButton prefabs to adjust layout, colors, and integrate the new icons.

--- Assets/_Project/Code/Runtime/UI/LevelInfo/SequenceSlotView.cs
+++ Assets/_Project/Code/Runtime/UI/LevelInfo/SequenceSlotView.cs
@@ -7,29 +7,29 @@ namespace UI.LevelInfo
     public class SequenceSlotView : MonoBehaviour
     {
         [SerializeField] private Image _icon;
-        
-        [Header("Add")]
-        [SerializeField] private GameObject _addRoot;
+
+        [Header("Add")] [SerializeField] private GameObject _addRoot;
         [SerializeField] private Button _addButton;
-        
-        [Header("Remove")]
-        [SerializeField] private GameObject _removeRoot;
+        [SerializeField] private GameObject _addIconBack;
+
+        [Header("Remove")] [SerializeField] private GameObject _removeRoot;
         [SerializeField] private Button _removeButton;
-        
+
         public void Initialize(ISequenceSlotViewPresenter presenter)
         {
             presenter.HasMovement.Subscribe((has) =>
             {
                 _addRoot.SetActive(!has);
+                _addIconBack.SetActive(has);
                 _removeRoot.SetActive(has);
             }).AddTo(this);
-            
+
             presenter.Icon.Subscribe((icon) =>
             {
                 _icon.enabled = icon != null;
                 _icon.sprite = icon;
             }).AddTo(this);
-            
+
             presenter.AddRequest.BindTo(_addButton).AddTo(this);
             presenter.RemoveRequest.BindTo(_removeButton).AddTo(this);
         }
