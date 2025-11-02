e60f5267e4f772efceaa49ac78bd2163e34fc720
Leonid Medvedev <77212731+LeonPucin@users.noreply.github.com>
2025-11-02 05:13:37 +0300

    Add HP bar and icon textures for UI
    Added new textures and metadata for HP bar background, plate, bar, and icon in the UI choose folder. Updated RageView.cs, PrayerChoosePage.prefab, and 01_Game.unity to integrate these assets. Minor update to Neucha-Regular SDF font asset.

--- Assets/_Project/Code/Runtime/UI/LevelInfo/RageView.cs
+++ Assets/_Project/Code/Runtime/UI/LevelInfo/RageView.cs
@@ -6,11 +6,13 @@ namespace UI.LevelInfo
 {
     public class RageView : MonoBehaviour
     {
-        [SerializeField] private Slider _slider;
+        //[SerializeField] private Slider _slider;
+        [SerializeField] private Image _image;
 
         public void Initialize(IRageViewPresenter presenter)
         {
-            presenter.Rage.Subscribe(val => _slider.value = val).AddTo(this);
+            //presenter.Rage.Subscribe(val => _slider.value = val).AddTo(this);
+            presenter.Rage.Subscribe(val => _image.fillAmount = 1 - val).AddTo(this);
         }
     }
 }
